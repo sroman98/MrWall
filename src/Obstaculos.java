a0f2b1fa765e2d1f2e6987b650f9fccb36b2f587
rcgc <rcgc140298@gmail.com>
2018-11-04 15:05:51 -0600

    actualizando clases obstaculos y obstaculos

--- src/Obstaculos.java
+++ src/Obstaculos.java
@@ -4,8 +4,27 @@ import java.util.ListIterator;
 
 public class Obstaculos {
 	private ArrayList obstaculos;
+	private Obstaculo obs1;
+	private Obstaculo obs2;
+	private Obstaculo obs3;
+	private Obstaculo obs4;
+	private Obstaculo obs5;
 	
-	Obstaculos(){obstaculos = new ArrayList<Obstaculo>(5);}
+	Obstaculos(){
+		obstaculos = new ArrayList<Obstaculo>(5);
+		
+		obs1 = new Obstaculo(30, 300, 30, "");
+		obs2 = new Obstaculo(70, 300, 30, "");
+		obs3 = new Obstaculo(110, 300, 30, "");
+		obs4 = new Obstaculo(150, 300, 30, "");
+		obs5 = new Obstaculo(190, 300, 30, "");
+		
+		obstaculos.add(obs1);
+		obstaculos.add(obs2);
+		obstaculos.add(obs3);
+		obstaculos.add(obs4);
+		obstaculos.add(obs5);
+	}
 	
 	public void paint(Graphics g) {
 		ListIterator<Obstaculo> apt = obstaculos.listIterator(); 
@@ -25,9 +44,7 @@ public class Obstaculos {
 		ListIterator<Obstaculo> apt = obstaculos.listIterator(); 
 		while(apt.hasNext()) {
 			Obstaculo obstaculo = apt.next();
-			if(obstaculo instanceof Obstaculo) {
-				obstaculos.remove(index);
-			}
+			if(obstaculo instanceof Obstaculo) {obstaculos.remove(index);}
 		}//While		
 	}
 }
